f159405f6d4c116282dc4eb3be8251f786bb4022
a lan <bluenowte97@gmail.com>
2018-10-11 17:50:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,17 +35,27 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
 }
 
 function removeElementFromBeginningOfArray(array, element) {
+  //removes array[0], starts from array [1]
   var newArr = array.slice(1);
   return newArr;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
+//removing last alters array just use pop()
   array.pop();
   return array;
 }
 
 function removeElementFromEndOfArray(array, element) {
-
+//slice takes up to last element
  var newArr = array.slice(0, array.length - 1);
   return newArr;
  }
+
+ var items = [1, 2, 3, 4, 5]
+ 
+// let's remove the third element
+ 
+// a slice from the start up to but not including index 2 (the third element)
+// and a slice from index 3 to the end
+[...items.slice(0, 2), ...items.slice(3)] // [1, 2, 4, 5]
